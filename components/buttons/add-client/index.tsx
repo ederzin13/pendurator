0ef343e79edab9c33d6974a500a358afd645ed7b
ederzin13 <edermurilo131313@gmail.com>
2026-08-12 18:05:21 -0300

    icon update

--- components/buttons/add-client/index.tsx
+++ components/buttons/add-client/index.tsx
@@ -1,16 +1,15 @@
 import { sen } from "@/assets/fonts/custom-font";
 import { spacing } from "@/constants/Spacing";
 import { typo } from "@/constants/Typography";
-import Octicons from "@expo/vector-icons/Octicons";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 export default function AddClient() {
   return (
     <TouchableOpacity style={styles.container}>
-      <Text style={styles.text}>
-        Novo <Octicons name="person-add" size={20} color="white" />
-      </Text>
+      <Text style={styles.text}>Novo</Text>
+      <FontAwesome6 name="plus" size={20} color="white" />
     </TouchableOpacity>
   );
 }
@@ -23,6 +22,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#ff7575",
     alignItems: "center",
     justifyContent: "center",
+    flexDirection: "row",
+    gap: spacing.innerPadding,
   },
 
   text: {
