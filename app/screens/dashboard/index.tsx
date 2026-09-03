fd2239d96149626fed51c7ac2d2c59e1bca22e27
ederzin13 <edermurilo131313@gmail.com>
2026-09-03 18:58:57 -0300

    logoff button on dashboard

--- app/screens/dashboard/index.tsx
+++ app/screens/dashboard/index.tsx
@@ -1,12 +1,17 @@
 import { sen } from "@/assets/fonts/custom-font";
+import Button from "@/components/buttons/button";
 import DashboardTab from "@/components/dashboard-screen/dashboard-tab";
 import FullScreen from "@/components/screen-wrappers/full-screen";
 import { typo } from "@/constants/Typography";
+import useAuth from "@/states/useAuth";
 import { router, Stack } from "expo-router";
 import { StyleSheet, Text } from "react-native";
 
 export default function DashboardScreen() {
+  const { clear } = useAuth();
+
   const handleBack = () => {
+    clear();
     router.back();
   };
 
@@ -16,7 +21,9 @@ export default function DashboardScreen() {
       <Text style={styles.title}>Bem vindo ao Pendurator!</Text>
       <DashboardTab title="Clientes" icon="person-running" />
       <DashboardTab title="Dívidas" />
-      {/* <Button label="Voltar" onClick={handleBack} /> */}
+
+      {/* {implementar mensagem de "deseja mesmo sair?"} */}
+      <Button label="Sair" onClick={handleBack} />
     </FullScreen>
   );
 }
